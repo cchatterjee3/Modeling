3afefaf1c1313ebfdf3800dbf11415bbe39dfa13
Chandim Chatterjee <chandimc2004@gmail.com>
2013-01-26 16:10:37 -0500

    First commit

---
+++ Chandim-Project/CommonDefs.h
@@ -0,0 +1,7 @@
+#ifndef __COMMON_DEFS_H__
+#define __COMMON_DEFS_H__
+
+
+typedef double Time_t
+
+#endif
---
+++ Chandim-Project/Events.h
@@ -0,0 +1,120 @@
+
+#ifndef __EVENTS_H__
+#define __EVENTS_H__
+
+#include "CommonDefs.h"
+
+class EventBase
+{
+      public:
+             EventBase (Time_t t) : time(t) {} 
+             virtual void CallHandler() = 0; // All subclasses must implement this
+             Time_t getTime()
+             {
+                  return time;     
+             }
+      private:
+              Time_t time; //TimeStamp for the event
+};
+
+template<typename T, typename OBJ>
+class Event0 : public EventBase
+{ // Event class with no arguments
+  // Type T is the object type for the event handler object
+  // Type OBJ is the actual event handler object
+public:
+  void (T::*handler)(void);
+  OBJ*      obj;
+public:
+  Event0(double t, void (T::*f)(), OBJ* obj0)
+    : EventBase(t), handler(f), obj(obj0){}
+  void CallHandler();
+};
+
+template <typename T, typename OBJ>
+void Event0<T, OBJ>::CallHandler()
+{
+  (obj->*handler)();
+}   
+
+// Event with one parameter on the callback function
+template<typename T, typename OBJ, typename U1, typename T1>
+class Event1 : public EventBase
+{
+public:
+  Event1(double t, void (T::*f)(U1), OBJ* obj0, T1 t1_0)
+    : EventBase(t), handler(f), obj(obj0), t1(t1_0){}
+  void (T::*handler)(U1);
+  OBJ*      obj;
+  T1        t1;
+public:
+  void CallHandler();
+};
+
+template <typename T, typename OBJ, typename U1, typename T1>
+void Event1<T, OBJ, U1, T1>::CallHandler()
+{
+  (obj->*handler)(t1);
+}
+
+template<typename T, typename OBJ,
+         typename U1, typename T1, 
+         typename U2, typename T2>
+class Event2 : public EventBase
+{
+public:
+  Event2(double t, void (T::*f)(U1, U2), OBJ* obj0, T1 t1_0, T2 t2_0)
+    : EventBase(t), handler(f), obj(obj0), t1(t1_0), t2(t2_0) {}
+  void (T::*handler)(U1, U2);
+  OBJ*      obj;
+  T1        t1;
+  T2        t2;
+public:
+  void CallHandler();
+};
+
+template <typename T, typename OBJ, 
+          typename U1, typename T1,
+          typename U2, typename T2>
+void Event2<T, OBJ, U1, T1, U2, T2>::CallHandler()
+{
+  (obj->*handler)(t1, t2);
+}
+
+template <typename T,  typename OBJ,
+          typename U1, typename T1,
+          typename U2, typename T2,
+          typename U3, typename T3>
+class Event3 : public EventBase {
+public:
+   Event3(double t, void (T::*f)(U1, U2, U3), OBJ *obj0, T1 t1_0, T2 t2_0, T3 t3_0)  
+     : EventBase(t), handler(f), obj(obj0), t1(t1_0), t2(t2_0), t3(t3_0) {}
+   void (T::*handler)(U1, U2, U3);
+   OBJ* obj;
+   T1 t1;
+   T2 t2;
+   T3 t3;
+   
+public:
+   void CallHandler();
+};
+
+template <typename T,  typename OBJ,
+          typename U1, typename T1,
+          typename U2, typename T2,
+          typename U3, typename T3> 
+void Event3<T,OBJ,U1,T1,U2,T2,U3,T3>::CallHandler() {
+     (obj->*handler)(t1,t2,t3);
+}
+
+//Event Comparator
+class event_compare
+{
+      public:
+             inline bool operator()(EventBase* const& l, const EventBase* const& r) const {
+                    if (l.getTime() < r.getTime()) return true;
+                    return false;       
+             }      
+}
+
+#endif
