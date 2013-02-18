c7d67b5de94126fa6d0ee5e050d561fb4c336af6
Piyush Sao <piyush3@gatech.edu>
2013-02-18 14:27:36 -0500

    postprocessing with c=doxy comments

--- Chandim-Project/PostProcessing.h
+++ Chandim-Project/PostProcessing.h
@@ -7,8 +7,15 @@
 #include "VehicleQueue.h"
 
 
-/*
+/** 
 * 	Takes exit queue and prints histogram of time takes to cover between source and destination
 * 	Also prints stats like, average time , standard deviation etc.
+*	@param EQ is exit Q
+*	@param buckets is number of buckets for histogram
+*	@param timeval is the  period of time which is divided into buckets 
+*	@param source is starting point of the journey
+*	@param dest is input for describing 
 */
-void PostProcStats(VehicleQueue* EQ, double TimeInterval, int buckets,int source, int dest);
+void PostProcStats(VehicleQueue* EQ, double timeval, int buckets,int source, int dest);
+
+
